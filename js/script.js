0f98a0472bdf4b83d46b0852a91c1e56822c4bc8
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2023-06-08 20:39:07 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -65,7 +65,7 @@ const members = {
         stay_cost       :4500,
         garage_cost     :0,
         running_payable :5218,
-        running_paid    :4500,
+        running_paid    :7000,
         running_meal    :9,
         // method calling
         previousAddedDining,
